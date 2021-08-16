f0884fe0faeb65c4134bd825770808ec0538b6a5
Wei <ywwei85@gmail.com>
2021-08-16 13:58:14 -0400

    Add api for landing page

--- Controllers/dataresource.controllers.js
+++ Controllers/dataresource.controllers.js
@@ -2,6 +2,13 @@ const logger = require("../Components/logger");
 const cache = require("../Components/cache");
 const config = require("../Config");
 const path = require("path");
+const dataresourceService = require("../Services/dataresource.service");
+
+const getLanding = (req, res) => {
+    const participatingResources = dataresourceService.getLanding();
+    res.json({status:"success", data: participatingResources});
+};
+
 
 const search = (req, res) => {
     const body = req.body;
@@ -23,6 +30,7 @@ const getById = (req, res) => {
 };
 
 module.exports = {
+    getLanding,
 	search,
 	getById
 };
--- Services/dataset.service.js
+++ Services/dataset.service.js
@@ -1,6 +1,6 @@
-import config from '../Config';
-import elasticsearch from './Components/elasticsearch';
-import cache from '../Components/cache';
+const config = require('../Config');
+const elasticsearch = require('../Components/elasticsearch');
+const cache = require('../Components/cache');
 
 const search = (query) => {
     return [];
